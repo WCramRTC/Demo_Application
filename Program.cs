73f319f24a50c2ca5290977d007edcd6ed65848c
WCramRTC <91401050+WCramRTC@users.noreply.github.com>
2023-12-11 10:36:48 -0800

    Change Message in Program

--- Program.cs
+++ Program.cs
@@ -4,7 +4,7 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Hello, Programming 122!");
 
         }
     }
